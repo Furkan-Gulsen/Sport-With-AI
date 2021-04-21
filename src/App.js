7f4479bbf7c0031b41fb70aac2fd7a4f5325f558
furkan-gulsen <m.furkangulsen@hotmail.com>
2021-04-21 23:43:47 +0300

    added react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import * as tf from "@tensorflow/tfjs"
+import * as posenet from "@tensorflow-models/posenet"
+import Webcam from "react-webcam"
 
 function App() {
   return (
