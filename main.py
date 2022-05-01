2ac48d023f9cc549493c5b58f2a1e77897522875
Furkan-Gulsen <m.furkangulsen@hotmail.com>
2022-05-01 12:56:59 +0300

    add requirements and fix bugs

--- main.py
+++ main.py
@@ -40,43 +40,47 @@ with mp_pose.Pose(min_detection_confidence=0.5,
     counter = 0  # movement of exercise
     status = True  # state of move
     while cap.isOpened():
-        ret, frame = cap.read()
-        # result_screen = np.zeros((250, 400, 3), np.uint8)
-
-        frame = cv2.resize(frame, (800, 480), interpolation=cv2.INTER_AREA)
-        ## recolor frame to RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame.flags.writeable = False
-        ## make detection
-        results = pose.process(frame)
-        ## recolor back to BGR
-        frame.flags.writeable = True
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
         try:
-            landmarks = results.pose_landmarks.landmark
-            counter, status = TypeOfExercise(landmarks).calculate_exercise(
-                args["exercise_type"], counter, status)
+            ret, frame = cap.read()
+            # result_screen = np.zeros((250, 400, 3), np.uint8)
+
+            frame = cv2.resize(frame, (800, 480), interpolation=cv2.INTER_AREA)
+            ## recolor frame to RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame.flags.writeable = False
+            ## make detection
+            results = pose.process(frame)
+            ## recolor back to BGR
+            frame.flags.writeable = True
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+                counter, status = TypeOfExercise(landmarks).calculate_exercise(
+                    args["exercise_type"], counter, status)
+            except:
+                pass
+
+            score_table(args["exercise_type"], counter, status)
+
+            ## render detections (for landmarks)
+            mp_drawing.draw_landmarks(
+                frame,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                mp_drawing.DrawingSpec(color=(255, 255, 255),
+                                    thickness=2,
+                                    circle_radius=2),
+                mp_drawing.DrawingSpec(color=(174, 139, 45),
+                                    thickness=2,
+                                    circle_radius=2),
+            )
+
+            cv2.imshow('Video', frame)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
         except:
-            pass
-
-        score_table(args["exercise_type"], counter, status)
-
-        ## render detections (for landmarks)
-        mp_drawing.draw_landmarks(
-            frame,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            mp_drawing.DrawingSpec(color=(255, 255, 255),
-                                   thickness=2,
-                                   circle_radius=2),
-            mp_drawing.DrawingSpec(color=(174, 139, 45),
-                                   thickness=2,
-                                   circle_radius=2),
-        )
-
-        cv2.imshow('Video', frame)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+            print("counter: " + str(counter))
             break
 
     cap.release()
